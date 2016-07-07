1b17ef76a506316647030069b487fdccc1197fe5
destijl <github@mailgreg.com>
2016-07-06 17:22:18 -0700

    asdf

--- vagrant/install_darwin.sh
+++ vagrant/install_darwin.sh
@@ -68,7 +68,7 @@ case $EUID in
   *)
     xcode_select
     install_homebrew
-    brew install protobuf
+    brew install homebrew/versions/protobuf260
     brew install libffi
     install_python
     install_python_deps
